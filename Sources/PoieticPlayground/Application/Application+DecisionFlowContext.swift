73919899c00c9e67f14291603e431988fb4184c8
Stefan Urbanek <stefan@agentfarms.net>
2026-09-12 23:24:08 +0200

    Added Application conformance to DecisionFlowContext

---
+++ Sources/PoieticPlayground/Application/Application+DecisionFlowContext.swift
@@ -0,0 +1,73 @@
+//
+//  Application+DecisionFlowContext.swift
+//  PoieticPlayground
+//
+//  Created by Stefan Urbanek on 12/09/2026.
+//
+
+extension Application: DecisionFlowContext {
+    func presentMessage(title: String, message: String, style: MessageStyle) {
+        let alert = ConfirmationDialog(
+            title: title,
+            message: message,
+            style: style,
+            options: [
+                DecisionOption("Dismiss")
+            ]
+        )
+        
+        queueDialog(alert)
+    }
+
+    func presentDecision(title: String, message: String, choices: [DecisionFlowChoice])
+    {
+        let options = choices.map { $0.option }
+        let dialog = ConfirmationDialog(title: title,
+                                        message: message,
+                                        options: options)
+        { option in
+            guard option >= 0 && option < choices.count else { return }
+            choices[option].action()
+        }
+        
+        queueDialog(dialog)
+                                        
+    }
+    
+    func presentFilePicker(title: String,
+                           mode: FileSelectionMode,
+                           filter: String?,
+                           completion: @escaping (String?) -> Void)
+    {
+        self.openFilePicker(title: title,
+                            mode: mode,
+                            filter: filter,
+                            callback: completion)
+    }
+
+    // Execute command immediately
+    func execute(_ command: any Command) throws (CommandError) {
+        guard let document else {
+            self.log("Trying to execute a command \(type(of: command)) without a document")
+            return
+        }
+        let context = CommandContext(app: self, document: document)
+        try command.run(context)
+    }
+    func queue(_ command: any Command) {
+        self.document?.queueCommand(command)
+    }
+    func presentSubflow(_ flow: any DecisionFlow) {
+        decisionManager.presentSubflow(flow)
+    }
+    func finish(_ flow: any DecisionFlow) -> Bool {
+        decisionManager.finish(flow)
+    }
+
+}
+
+extension Application {
+    func run(_ flow: DecisionFlow) {
+        self.decisionManager.start(flow)
+    }
+}
