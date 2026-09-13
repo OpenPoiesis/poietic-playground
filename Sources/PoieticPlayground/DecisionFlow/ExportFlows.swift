3db3260c3106a58b0e8d4a743987aacfadbd4642
Stefan Urbanek <stefan@agentfarms.net>
2026-09-13 10:24:16 +0200

    Added reusable ChoosePathForWritingFlow

--- Sources/PoieticPlayground/DecisionFlow/ExportFlows.swift
+++ Sources/PoieticPlayground/DecisionFlow/ExportFlows.swift
@@ -7,47 +7,43 @@
 
 import Foundation
 
-// TODO: Extract present selector -> check for existence -> confirm overwrite to a single. Requires path outcome
-
-final class ExportSVGFlow: DecisionFlow {
-    weak let context: (any DecisionFlowContext)?
-
-    init(context: any DecisionFlowContext) {
+final class ChoosePathForWritingFlow: DecisionFlow {
+    private weak let context: (any DecisionFlowContext)?
+    private let title: String
+    private let fileExtension: String
+    
+    private(set) var selectedURL: URL? = nil
+    
+    init(context: any DecisionFlowContext, title: String, fileExtension: String) {
+        self.title = title
         self.context = context
+        self.fileExtension = fileExtension
     }
     
     func start() {
         guard let context else { return }
-        context.presentFileSelector(title: "Export SVG",
+        context.presentFileSelector(title: title,
                                     mode: .save,
-                                    filter: "*.svg",
+                                    filter: "*." + fileExtension,
                                     completion: self.pathSelected)
     }
     
     func pathSelected(selectedPath: String?) {
         guard let context else { return }
-        
         guard let selectedPath else {
             context.finish(self, outcome: .cancelled)
             return
         }
         
-        let url = Document.normalisePathExtension(URL(fileURLWithPath: selectedPath), extension: "svg")
+        let url = Document.normalisePathExtension(URL(fileURLWithPath: selectedPath),
+                                                  extension: fileExtension)
         
-        if FileManager.default.fileExists(atPath: url.path) {
-            self.confirmOverwrite(url: url)
+        guard FileManager.default.fileExists(atPath: url.path) else {
+            self.selectedURL = url
+            context.finish(self, outcome: .success)
+            return
         }
-        else {
-            let command = ExportSVGCommand(url: url, appendExtensionIfNeeded: true)
-            do {
-                try context.execute(command)
-                context.finish(self, outcome: .success)
-            }
-            catch {
-                context.presentMessage(title: "Export Failed", message: error.message, style: .error)
-                context.finish(self, outcome: .failure)
-            }
-        }
+        confirmOverwrite(url: url)
     }
     
     func confirmOverwrite(url: URL) {
@@ -60,10 +56,51 @@ final class ExportSVGFlow: DecisionFlow {
                     context.finish(self, outcome: .cancelled)
                 },
                 DecisionFlowChoice("Overwrite", emphasis: .destructive) {
-                    let outcome = saveDocument(to: url, context: context)
-                    context.finish(self, outcome: outcome)
+                    self.selectedURL = url
+                    context.finish(self, outcome: .success)
                 }
             ]
         )
+    }
+}
+
+final class ExportSVGFlow: DecisionFlow {
+    weak let context: (any DecisionFlowContext)?
+
+    init(context: any DecisionFlowContext) {
+        self.context = context
+    }
+    
+    func start() {
+        guard let context else { return }
+        
+        let choosePath = ChoosePathForWritingFlow(context: context, title: "Export SVG", fileExtension: "svg")
+        context.presentSubflow(choosePath) { [weak self] outcome in
+            guard let self else { return }
+
+            switch outcome {
+            case .success:
+                guard let url = choosePath.selectedURL else {
+                    context.finish(self, outcome: .cancelled)
+                    break
+                }
+                self.export(to: url)
+            case .cancelled, .failure:
+                context.finish(self, outcome: outcome)
+            }
+        }
+    }
+    func export(to url: URL) {
+        guard let context else { return }
+        
+        let command = ExportSVGCommand(url: url, appendExtensionIfNeeded: true)
+        do {
+            try context.execute(command)
+            context.finish(self, outcome: .success)
+        }
+        catch {
+            context.presentMessage(title: "Export Failed", message: error.message, style: .error)
+            context.finish(self, outcome: .failure)
+        }
     }
 }
